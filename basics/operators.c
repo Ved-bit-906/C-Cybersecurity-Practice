dd07b932505f3d1a2c067ad38350d04b6450809d
Vedanth M B <vedanthvinu15@gmail.com>
2026-08-30 10:34:10 +0530

    Add basic operators demonstration in C

---
+++ basics/operators.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("\n--- Arithmetic Operators ---\n");
+    printf("Addition: %d\n", a + b);
+    printf("Subtraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+    printf("Division: %d\n", a / b);
+    printf("Remainder: %d\n", a % b);
+
+    printf("\n--- Relational Operators ---\n");
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n", a != b);
+    printf("a > b: %d\n", a > b);
+    printf("a < b: %d\n", a < b);
+
+    printf("\n--- Logical Operators ---\n");
+    printf("(a > 0 && b > 0): %d\n", a > 0 && b > 0);
+    printf("(a > 0 || b > 0): %d\n", a > 0 || b > 0);
+    printf("!(a > 0): %d\n", !(a > 0));
+
+    return 0;
+}
